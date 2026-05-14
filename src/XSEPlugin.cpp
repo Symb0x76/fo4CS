b021121b047225a07c6762a13bea24d80574e8a7
Symb0x76 <9667434@qq.com>
2026-05-14 08:52:33 +0800

    chore: remove unused XSEPlugin.cpp (replaced by plugin-specific entries)

--- src/XSEPlugin.cpp
+++
@@ -1,6 +0,0 @@
-#include "PluginCommon.h"
-
-#if 0
-// Legacy single-plugin entry kept temporarily for reference during the split.
-// New entry points live in FrameGenPlugin.cpp and UpscalerPlugin.cpp.
-#endif
